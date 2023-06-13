01c2cc903234fa7b52fd193018d0bb2ff81c7d05
personryan <mageryan1@gmail.com>
2023-06-13 17:50:01 +0800

    Update sendEmail.js

--- functions/sendEmail.js
+++ functions/sendEmail.js
@@ -1,21 +1,45 @@
 const express = require('express');
-const app = express();
+const nodemailer = require('nodemailer');
 
+const app = express();
 app.use(express.json());
 
-app.post('/submit-form', (req, res) => {
-  // Retrieve form data from the request body
-  const { name, email, message } = req.body;
+app.post('/submit-form', async (req, res) => {
+  try {
+    // Retrieve form data from the request body
+    const { name, email, message } = req.body;
 
-  // Compose email content using the form data
-  const subject = 'Inquiry for SIT Research Group';
-  const body = `Dear SIT Research Group,\n I am${name}.\n ${message}\n Best Regards, ${name}`;
+    // Compose email content using the form data
+    const subject = 'Inquiry for SIT Research Group';
+    const body = `Name: ${name},\n\nEmail: ${email}.\n\n${message}\n\n`;
 
-  // Code to send the email using your chosen email library or service
+    // Create a transporter with your email service provider configuration
+    const transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+        user: 'mageryan1@gmail.com',
+        pass: 'fabuliser28',
+      },
+    });
 
-  // Placeholder response
-  res.status(200).send('Email sent successfully!');
+    // Define the email options
+    const mailOptions = {
+      from: 'mageryan1@gmail.com',
+      to: 'mageryan1@gmail.com',
+      subject,
+      text: body,
+    };
+
+    // Send the email
+    await transporter.sendMail(mailOptions);
+
+    // Return a success response
+    res.status(200).send('Email sent successfully!');
+  } catch (error) {
+    // Handle any errors
+    console.error('Error sending email:', error);
+    res.status(500).send('Error sending email.');
+  }
 });
 
 module.exports = app;
-
